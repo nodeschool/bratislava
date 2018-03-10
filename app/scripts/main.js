11ad249cbf4b147389e6b0818382df841840bdf5
Matej Nemček <ybdaba@gmail.com>
2018-03-10 15:42:39 +0100

    updated address

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,14 @@ import GMaps from 'gmaps';
 
 const map = new GMaps({
   el: '#map',
-  lat: 48.1442774,
-  lng: 17.1066003,
+  lat: 48.144558,
+  lng: 17.115472,
   scrollwheel: false,
 });
 
 map.addMarker({
-  lat: 48.1442774,
-  lng: 17.1066003,
+  lat: 48.144558,
+  lng: 17.115472,
   title: 'Nodeschool - Bratislava',
   infoWindow: {
     content: '<p>Nodeschool - Bratislava</p>',
